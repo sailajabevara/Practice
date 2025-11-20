2c0d614c74867cc723072728e611bed3213f6019
sailajabevara <155553126+sailajabevara@users.noreply.github.com>
2025-11-20 20:28:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        
+        if(s1.length() != s2.length())
+        {
+            return false;
+        }
+        else
+        {
+             char[] c1 = s1.toCharArray();
+             char[] c2 = s2.toCharArray();
+             
+             Arrays.sort(c1);
+             Arrays.sort(c2);
+             
+             return Arrays.equals(c1, c2);
+        }
+       
+    }
+}
