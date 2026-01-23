8c89acfc830efa21ea081f5989016cac48ce3ba5
sailajabevara <155553126+sailajabevara@users.noreply.github.com>
2026-01-23 18:21:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -0,0 +1,16 @@
+class Solution {
+    static boolean isEven(int n) {
+        
+        if (n%2==0){
+            // System.out.println("true");
+            return true;
+            
+        }
+        else{
+            // System.out.println("false");
+            return false;
+        }
+
+
+    }        
+}
