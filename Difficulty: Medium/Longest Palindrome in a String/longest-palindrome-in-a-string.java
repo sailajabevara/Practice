f01174aaab3b4d36f39693cc1c4a33f70a6f744b
sailajabevara <155553126+sailajabevara@users.noreply.github.com>
2026-02-07 22:07:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    static String longestPalindrome(String s) {
+        // code here
+        int n=s.length();
+        int len, maxLen=0;
+        String ans="";
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPallin(s,i,j)){
+                    len=j-i+1;
+                    maxLen=Math.max(maxLen,len);
+                    if(maxLen==len && ans.length()!=len){
+                        ans=s.substring(i,j+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    
+    static boolean isPallin(String s, int start, int end){
+        while(start<=end){
+            if(s.charAt(start)!=s.charAt(end))
+            return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
